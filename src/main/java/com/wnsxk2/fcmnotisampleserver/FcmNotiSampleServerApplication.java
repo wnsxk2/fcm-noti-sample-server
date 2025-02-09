0aa3f30bd8bd5747160977a4a703d15be8e0d9bc
wnsxk2 <wnsxk206@gmail.com>
2025-02-09 20:18:11 +0900

    project init

---
+++ src/main/java/com/wnsxk2/fcmnotisampleserver/FcmNotiSampleServerApplication.java
@@ -0,0 +1,13 @@
+package com.wnsxk2.fcmnotisampleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmNotiSampleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcmNotiSampleServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wnsxk2/fcmnotisampleserver/FcmNotiSampleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wnsxk2.fcmnotisampleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmNotiSampleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
